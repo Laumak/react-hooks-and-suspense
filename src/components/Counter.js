e93a44f412f8768c2865f70d0660212efc013e9f
Lauri Mäkinen <lauri@mits.fi>
2019-06-16 10:59:56 +0300

    Use React's "useEffect" function to handle side-effects

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 class ComponentCounter extends React.Component {
   state = { count: 0 };
@@ -14,14 +14,20 @@ class ComponentCounter extends React.Component {
   }
 }
 
+// https://egghead.io/playlists/react-hooks-and-suspense-650307f2
 const useCounter = ({ initialState, step }) => {
-  const [count, setCount] = useState(initialState);
+  // In order to only run this once, it must be a function
+  const initialCount = () =>
+    Number(localStorage.getItem("count") || initialState);
+  const [count, setCount] = useState(initialCount);
   const increment = () => setCount(count + step);
+  useEffect(() => {
+    localStorage.setItem("count", count);
+  }, [count]); // Only "run" this effect when the given value(s) change
 
   return { count, increment };
 };
 
-// https://egghead.io/lessons/react-use-the-usestate-react-hook?pl=react-hooks-and-suspense-650307f2
 const UseStateCounter = () => {
   const { count, increment } = useCounter({ initialState: 5, step: 3 });
   return <button onClick={increment}>{count}</button>;
